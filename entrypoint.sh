adf066cdf42163ad58f8c13261e9071212200356
Martin Bhuong <martin@genius.ke>
2024-03-19 13:51:09 +0100

    implement apace restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Enable mod_rewrite
+a2enmod rewrite
+
+# Restart Apache
+service apache2 restart
+
+# Fix to really stop Apache
+pkill -9 apache
+
 # Execute the database setup script
 /usr/local/bin/db_setup.sh
 
-# Start Apache or any other services
+# Start Apache
 apachectl -D FOREGROUND
-#exec apache2-foreground
