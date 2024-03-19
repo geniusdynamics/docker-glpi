e3c4b7d22b6318290751c59bcbda64dd2e22c3ca
Martin Bhuong <martin@genius.ke>
2024-03-19 17:08:10 +0100

    implement apache config in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,16 @@
 /usr/local/bin/db_setup.sh
 
 
+# Set default Apache configuration
+echo -e "<VirtualHost *:80>\n\tDocumentRoot /var/www/html/glpi\n\n\t<Directory /var/www/html/glpi>\n\t\tAllowOverride All\n\t\tOrder Allow,Deny\n\t\tAllow from all\n\t</Directory>\n\n\tErrorLog /var/log/apache2/error-glpi.log\n\tLogLevel warn\n\tCustomLog /var/log/apache2/access-glpi.log combined\n</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
+
+# Check for conditions to modify the Apache configuration
+# For example, check if a specific file or directory exists
+if [ -d /var/www/html/glpi/public ]; then
+    # Modify Apache configuration for a public root directory
+    sed -i 's#/var/www/html/glpi#/var/www/html/glpi/public#g' /etc/apache2/sites-available/000-default.conf
+fi
+
 # Enable mod_rewrite
 a2enmod rewrite
 
