2121ab6db654e47fbb783e74aae50a7ab5917185
kemboi22 <kemboielvis22@gmail.com>
2024-03-22 02:51:03 +0300

    Enhance Apache configuration in entrypoint.sh to support GLPI public directory with improved rewrite rules and authorization header handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,33 @@ echo "ServerName $IP_ADDRESS" >> /etc/apache2/apache2.conf
 
 
 # Set default Apache configuration
-echo -e "<VirtualHost *:80>\n\tDocumentRoot /var/www/html/glpi\n\n\t<Directory /var/www/html/glpi>\n\t\tAllowOverride All\n\t\tOrder Allow,Deny\n\t\tAllow from all\n\t</Directory>\n\n\tErrorLog /var/log/apache2/error-glpi.log\n\tLogLevel warn\n\tCustomLog /var/log/apache2/access-glpi.log combined\n</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
+#echo -e "<VirtualHost *:80>\n\tDocumentRoot /var/www/html/glpi\n\n\t<Directory /var/www/html/glpi>\n\t\tAllowOverride All\n\t\tOrder Allow,Deny\n\t\tAllow from all\n\t</Directory>\n\n\tErrorLog /var/log/apache2/error-glpi.log\n\tLogLevel warn\n\tCustomLog /var/log/apache2/access-glpi.log combined\n</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
+echo -e "<VirtualHost *:80>
+    DocumentRoot /var/www/html/glpi
 
+    <Directory /var/www/html/glpi>
+        AllowOverride All
+        Order Allow,Deny
+        Allow from all
+    </Directory>
+    <Directory /var/www/glpi/public>
+        Require all granted
+
+        RewriteEngine On
+
+        # Ensure authorization headers are passed to PHP.
+        # Some Apache configurations may filter them and break usage of API, CalDAV, ...
+        RewriteCond %{HTTP:Authorization} ^(.+)$
+        RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+
+        # Redirect all requests to GLPI router, unless file exists.
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteRule ^(.*)$ index.php [QSA,L]
+    </Directory>
+    ErrorLog /var/log/apache2/error-glpi.log
+    LogLevel warn
+    CustomLog /var/log/apache2/access-glpi.log combined
+</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
 # Check for conditions to modify the Apache configuration
 # For example, check if a specific file or directory exists
 #if [ -d /var/www/html/glpi/public ]; then
